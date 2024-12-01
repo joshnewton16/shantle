12b817ebd082235bd2984fa6705bb8a6efd7dd6e
joshnewton16 <joshnewton16@gmail.com>
2024-11-30 21:00:51 -0500

    Added missing pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import ShantyGame from '../components/ShantyGame'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <ShantyGame />
+    </div>
+  )
+}
